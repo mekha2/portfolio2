a56555592cb5cd78b866028d866cfe5f06d5d3ea
mekha2 <144302450+mekha2@users.noreply.github.com>
2024-12-13 14:02:25 +0200

    Update gui.py

--- gui.py
+++ gui.py
@@ -38,12 +38,12 @@ label.pack(pady=10)
 text_entry = tk.Entry(root, width=30)
 text_entry.pack(pady=5)
 
-# شريط التحكم بسرعة الصوت
+
 # speed_label = tk.Label(root, text="Adjust Speed:", font=("Arial", 10))
 # speed_label.pack(pady=5)
 
 # speed_slider = tk.Scale(root, from_=50, to=300, orient="horizontal", command=change_speed)
-# speed_slider.set(150)  # القيمة الافتراضية
+# speed_slider.set(150)  
 # speed_slider.pack(pady=5)
 
 button_frame=tk.Frame(root)
